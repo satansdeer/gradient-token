357ab06ad8f3e42d7af45088e9d5f71a700099c1
Maksim Ivanov <satansdeer@gmail.com>
2018-04-02 17:56:03 +0300

    Add basic front (#3)
    * Update gitignore
    
    * Add basic front
    
    * Save deployed contract address for front

--- migrations/2_deploy_contract.js
+++ migrations/2_deploy_contract.js
@@ -1,5 +1,17 @@
 const GradientToken = artifacts.require("GradientToken");
+const util = require("util");
+const fs = require("fs");
+const path = require("path");
+const writeFile = util.promisify(fs.writeFile);
 
 module.exports = async function(deployer) {
   const gradientToken = await deployer.deploy(GradientToken);
+  const addresses = {
+    tokenAddress: GradientToken.address
+  };
+
+  await writeFile(
+    path.join(__dirname, "..", "front", "src", "addresses.json"),
+    JSON.stringify(addresses)
+  );
 };
