c4ac43b5988b66e3be12955275b8685c16e6960d
Maksim Ivanov <maxim.ivanov@toptal.com>
2018-04-02 18:56:15 +0300

    Add styles

--- front/src/App.js
+++ front/src/App.js
@@ -1,13 +1,16 @@
 import React, { Component } from "react";
 import { Provider } from "mobx-react";
 import stores from "./stores";
+import TokensPage from "./TokensPage";
 import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <Provider {...stores}>
-        <div className="App" />
+        <div className="App">
+          <TokensPage />
+        </div>
       </Provider>
     );
   }
--- front/src/index.js
+++ front/src/index.js
@@ -1,8 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import registerServiceWorker from './registerServiceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import registerServiceWorker from "./registerServiceWorker";
+import WebFont from "webfontloader";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+WebFont.load({
+  google: {
+    families: ["Nunito:300,400,700", "Open Sans:300,400,700", "sans-serif"]
+  }
+});
+
+ReactDOM.render(<App />, document.getElementById("root"));
 registerServiceWorker();
