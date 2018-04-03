3a1ec40b58159cb44908e67361bb801a55023c0d
Maksim Ivanov <maxim.ivanov@toptal.com>
2018-04-03 06:59:50 +0300

    Hotfix, add Modal to App

--- front/src/App.js
+++ front/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from "react";
 import { Provider } from "mobx-react";
-import stores from "./stores";
+import Modal from "components/Modal";
 import TokensPage from "./TokensPage";
+import stores from "./stores";
 import "./App.css";
 
 class App extends Component {
@@ -9,6 +10,7 @@ class App extends Component {
     return (
       <Provider {...stores}>
         <div className="App">
+          <Modal />
           <TokensPage />
         </div>
       </Provider>
